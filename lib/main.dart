3733c14c3b97fdd4a93af8c37bffd37774233719
krisnachy <cahyayuana@gmail.com>
2021-06-01 07:00:20 +0700

    First Flutter

---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Latihan Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello, world!'),
+        ),
+        body: Center(
+          //child: Text('Hello, world!'),
+
+          //Mengubah widget text dengan class heading
+          // child: Heading(
+          //   text: "Hello World!",
+          // ),
+
+          //Mengubah widget dengan Statefull Widget
+          child: BiggerText(
+            text: "Hello World!!",
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Heading extends StatelessWidget {
+  final String text; //state bersifat final
+
+  Heading({this.text}); //state masuk ke dalam constructor
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: 24.0,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+}
+
+class BiggerText extends StatefulWidget {
+  final String text;
+
+  const BiggerText({this.text});
+
+  @override
+  _BiggerTextState createState() => _BiggerTextState();
+}
+
+class _BiggerTextState extends State<BiggerText> {
+  double _textSize = 16.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(widget.text, style: TextStyle(fontSize: _textSize)),
+        ElevatedButton(
+          child: Text("Perbesar"),
+          onPressed: () {
+            setState(() {
+              _textSize = 32.0;
+            });
+          },
+        )
+      ],
+    );
+  }
+}
